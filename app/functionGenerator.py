7c6dbea4b02ee14a604158264b0e607e64170586
matheusburatti <matheusburatti00@gmail.com>
2023-06-25 19:39:15 -0300

    testes functionGenerator

--- app/functionGenerator.py
+++ app/functionGenerator.py
@@ -95,14 +95,13 @@ class functionGenerator:
     def generateSineSumWave(input: str):
         # ===== Pegando a função a partir da entrada =====
         functions = cosSineSumParser.getFunc(input)
-        print(functions)
         max_ang_freq = max(
             functions, key=lambda item: item[3])[2]
         freq = max_ang_freq/(2*math.pi)
-        periodo = 4*math.pi/np.gcd.reduce(
+        periodo = 2*math.pi/np.gcd.reduce(
             [int(x[2]) for x in functions])
         # ===== Calculando a quantidade de pontos =====
-        qtd_pontos = 32*freq if freq < 10000 else 32000
+        qtd_pontos = 128
         t = np.linspace(0, periodo, qtd_pontos)
         F = list()
         for function in functions:
@@ -115,7 +114,7 @@ class functionGenerator:
         return Function(Waveform.SINESUM, freq, 0, 1, 1, pontos, qtd_pontos)
 
     def generateSawWave(freq: float, dutyCycle: float):
-        qtd_pontos = 32
+        qtd_pontos = 128
         pontos = np.zeros(qtd_pontos, dtype=int)
         slice = np.r_[0:int(dutyCycle*qtd_pontos)]
         pontos[slice] = np.poly1d([4095/(dutyCycle*qtd_pontos), 0])(slice)
@@ -124,7 +123,7 @@ class functionGenerator:
 
     def generateSquareWave(freq: float, dutyCycle: float):
         # Duty cycle na forma 0.200 - 0.800
-        qtd_pontos = 32
+        qtd_pontos = 64
         pontos = np.zeros(qtd_pontos, dtype=int)
         slice = np.r_[0:int(dutyCycle*qtd_pontos)]
         pontos[slice] = 4095
@@ -132,7 +131,7 @@ class functionGenerator:
         return Function(Waveform.SQUARE, freq, 0, 1, dutyCycle, pontos, qtd_pontos)
 
     def generateTriangleWave(freq: float):
-        qtd_pontos = 32
+        qtd_pontos = 64
         pontos = np.zeros(qtd_pontos, dtype=int)
         first_slice = np.r_[0:int(0.5*qtd_pontos)]
         second_slice = np.r_[int(0.5*qtd_pontos):qtd_pontos]
@@ -151,3 +150,57 @@ class functionGenerator:
         pontos = [round(elem)
                   for elem in 4095*(F+1)/2]
         return Function(Waveform.SINE, freq, phase, amplitude, 1, pontos, qtd_pontos)
+
+
+# Testing and plotting output of the function generator
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    # Test 1: Sine wave
+    plt.figure()
+    plt.plot(functionGenerator.generateSineWave(
+        1000, 0, 1).pontos, 'r', label='freq = 1000Hz')
+    plt.plot(functionGenerator.generateSineWave(
+        500, 0, 1).pontos, 'b', label='freq = 500Hz')
+    plt.plot(functionGenerator.generateSineWave(
+        2000, 0, 1).pontos, 'g', label='freq = 2000Hz')
+    plt.show()
+
+    # Test 2: Sawtooth wave
+    plt.figure()
+    plt.plot(functionGenerator.generateSawWave(2000, 0.5).pontos,
+             'r', label='Duty cycle = 0.5 & freq = 2000Hz')
+    plt.plot(functionGenerator.generateSawWave(1000, 0.25).pontos,
+             'b', label='Duty cycle = 0.25 & freq = 1000Hz')
+    plt.plot(functionGenerator.generateSawWave(500, 0.8).pontos,
+             'g', label='Duty cycle = 0.8 & freq = 500Hz')
+    plt.show()
+
+    # Teste 3: Square wave
+    plt.figure()
+    plt.plot(functionGenerator.generateSquareWave(2000, 0.5).pontos,
+             'r', label='Duty cycle = 0.5 & freq = 2000Hz')
+    plt.plot(functionGenerator.generateSquareWave(1000, 0.2).pontos,
+             'b', label='Duty cycle = 0.2 & freq = 1000Hz')
+    plt.plot(functionGenerator.generateSquareWave(500, 0.8).pontos,
+             'g', label='Duty cycle = 0.8 & freq = 500Hz')
+    plt.show()
+
+    # Teste 4: Triangle wave
+    plt.figure()
+    plt.plot(functionGenerator.generateTriangleWave(
+        2000).pontos, 'r', label='freq = 2000Hz')
+    plt.plot(functionGenerator.generateTriangleWave(
+        1000).pontos, 'b', label='freq = 1000Hz')
+    plt.plot(functionGenerator.generateTriangleWave(
+        500).pontos, 'g', label='freq = 500Hz')
+    plt.show()
+
+    # Teste 5: Sine sum wave
+    plt.figure()
+    plt.plot(functionGenerator.generateSineSumWave(
+        "2cos(1000t + 0) + 3sin(2000t + 0)").pontos, 'r', label='2cos(1000t + 0) + 3sin(2000t + 0)')
+    plt.plot(functionGenerator.generateSineSumWave("2cos(1000t + 0) + 3sin(2000t + 0) + 4cos(3000t + 0)").pontos,
+             'b', label='2cos(1000t + 0) + 3sin(2000t + 0) + 4cos(3000t + 0)')
+    plt.plot(functionGenerator.generateSineSumWave("2cos(1000t + 0) + 3sin(2000t + 0) + 4cos(3000t + 0) + 5sin(4000t + 0)").pontos,
+             'g', label='2cos(1000t + 0) + 3sin(2000t + 0) + 4cos(3000t + 0) + 5sin(4000t + 0)')
+    plt.show()
